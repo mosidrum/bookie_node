0c3a38c9eb527219c1a3dec70634b44486d988ac
mosidrum <mosiokanga@gmail.com>
2024-05-09 15:11:37 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, {Express} from 'express';
+import bodyParser from "body-parser";
+import cors from 'cors'
+
+const app: Express = express()
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/api', (req, res, next) => {
+    return res.status(200).json({
+        message: "Hello world",
+    });
+})
+export default app;
